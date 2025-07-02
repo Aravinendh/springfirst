d701824533484b4f47017b6558f55b8c07254c87
Aravinendh <aravinendhr@gmail.com>
2025-07-02 15:59:57 +0530

    first commit

---
+++ src/main/java/org/example/SpringBoot.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class SpringBoot {
+    SpringBoot() {
+        System.out.println("Students are attending SpringBoot inside CodeStudio");
+    }
+    public void hello(){
+        System.out.println("Training ended successfully");
+    }
+}
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,9 @@
+package org.example;
+public class Student {
+    public Student(){
+        System.out.println("Inside Student Class!");
+    }
+    public void internship(){
+        System.out.println("Students are attending springboot training program at code studio!");
+    }
+}
